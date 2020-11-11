dcc8c3a0bf7f4085fce8d690ae5f7e32a58c2d75
Adam Alzuhairi <adam_alzu@hotmail.com>
2020-11-11 15:54:34 +0100

    Added functional buttom and resource text

---
+++ Assets/Scripts/Nuttap.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Nuttap : MonoBehaviour
+{
+
+    public Resource resource;
+    public int produceAmount;
+    public Text resValue;
+    
+    void Start()
+    {
+        this.resource.ResourceAmount = PlayerPrefs.GetInt(resource.name);
+    }
+    
+    void Update()
+    {
+        this.resValue.text = resource.ResourceAmount.ToString("0 nuts");
+    }
+
+    public void ProduceNuts()
+    {
+        this.resource.AddResource(produceAmount);
+    }
+    
+    
+}
