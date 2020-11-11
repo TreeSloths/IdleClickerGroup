57950bd303efefc3b733b36032a2df466b9a3470
Kevin Lempa <kevin.lempa@student.forsbergsskola.se>
2020-11-11 13:02:11 +0100

    adds Resources
    adds nuts and GoldenNuts as a resource

---
+++ Assets/Scripts/Resource.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class Resource : ScriptableObject {
+    
+    private int _resourceAmount;
+
+    public int ResourceAmount {
+        get => PlayerPrefs.GetInt(name, 0);
+        set => PlayerPrefs.SetInt(name, value);
+    }
+    
+    
+    public void AddResource(int addAmount) {
+        if (addAmount > 0)
+            ResourceAmount += addAmount;
+    }
+
+    public void ReduceResource(int reduceBy) {
+        if (ResourceAmount < reduceBy) {
+            return;
+        } else ResourceAmount -= reduceBy;
+    }
+}
