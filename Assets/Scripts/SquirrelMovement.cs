1586cf551016a70e796ff79945766ad4241c395c
Jimmie Lundin <jimmie.lundin@student.forsbergsskola.se>
2020-11-12 09:34:12 +0100

    Started movement script
    testing movement

---
+++ Assets/Scripts/SquirrelMovement.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public float speed;
+    private bool movingRight = true;
+   
+    void Update()
+    {
+        transform.Translate(Vector2.right * speed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collision2D other)
+    {
+        if (movingRight == true)
+        {
+            transform.eulerAngles = new Vector3(0, 180, 0);
+            movingRight = false;
+        }
+        else
+        {
+            transform.eulerAngles = new Vector3(0, 0, 0);
+            movingRight = true;
+        }
+    }
+}
