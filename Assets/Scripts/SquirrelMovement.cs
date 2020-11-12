4c4f6b41ae3486a5d2d1ea1b6947e57604670159
Jimmie Lundin <jimmie.lundin@student.forsbergsskola.se>
2020-11-12 13:48:35 +0100

    Rolling Sqirrels
    prefab rolling squirrle

--- Assets/Scripts/SquirrelMovement.cs
+++ Assets/Scripts/SquirrelMovement.cs
@@ -7,24 +7,35 @@ public class SquirrelMovement : MonoBehaviour
 {
     public float speed;
     private bool movingRight = true;
+    private bool collided;
    
+    public void Movement() 
+    {
+        if (collided) 
+        {
+            transform.Translate(Vector2.right * speed * Time.deltaTime, Space.World);
+        } else if (!collided) transform.Translate(Vector2.left * speed * Time.deltaTime, Space.World);
+    }
+    
     void Update()
     {
-        transform.Translate(Vector2.right * speed * Time.deltaTime);
         transform.Rotate(new Vector3(0,0,360) * speed * Time.deltaTime);
+        Movement();
     }
 
-    private void OnCollisionEnter2D(Collision2D other)
+    private void OnTriggerEnter2D(Collider2D other)
     {
         if (movingRight == true)
         {
             transform.eulerAngles = new Vector3(0, 180, 0);
             movingRight = false;
+            collided = true;
         }
         else
         {
             transform.eulerAngles = new Vector3(0, 0, 0);
             movingRight = true;
+            collided = false;
         }
     }
 }
