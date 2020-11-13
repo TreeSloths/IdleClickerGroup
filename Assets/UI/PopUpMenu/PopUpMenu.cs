6a9fe3af35cd8305deaa9f7fb3ac650faf4ba174
Adam Alzuhairi <adam_alzu@hotmail.com>
2020-11-13 15:08:36 +0100

    Unfinished prototype popupmenu

---
+++ Assets/UI/PopUpMenu/PopUpMenu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PopUpMenu : MonoBehaviour
+{
+
+    public Text popUpMenuText;
+    public SaveTime SaveTime;
+    
+    
+    void Start()
+    {
+        this.popUpMenuText.text = "Test";  // whatever the fuck, waiting for next week and i will add what values shall be shown
+    }
+    
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/UI/PopUpMenu/PopUpMenuButtom.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class PopUpMenuButtom : MonoBehaviour
+{
+
+    public GameObject popUpMenu;
+    
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        
+    }
+
+    public void ClosePopUp()
+    {
+        popUpMenu.SetActive(false);
+    }
+    
+}
