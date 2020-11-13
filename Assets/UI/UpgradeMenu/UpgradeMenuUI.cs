0da49e5175ebe1907638364cda44a02f67db565a
Oskar Jansson <oskar_slim89@hotmail.com>
2020-11-13 09:31:59 +0100

    pushing UI files again

---
+++ Assets/UI/UpgradeMenu/UpgradeMenuUI.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpgradeMenuUI : MonoBehaviour
+{
+    float offsetX = -10.0f;
+    float minX = -10.0f;
+    
+    bool closeMenu = false;
+
+
+    private bool anticipation = true;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        var rect = GetComponent<RectTransform>();
+        rect.offsetMax = Vector2.zero;
+        rect.offsetMin = Vector2.zero;
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (closeMenu) PanAway();
+    }
+
+    public void CloseMenu()
+    {
+        var StartVector = new Vector2(50,0);
+        var rect = GetComponent<RectTransform>();
+        rect.offsetMax -= StartVector;
+        rect.offsetMin -= StartVector;
+        closeMenu = true;
+       // Destroy(gameObject);
+    }
+    
+    void PanAway()
+    {
+        
+        var VectorOffset = new Vector2(offsetX, 0);
+        var rect = GetComponent<RectTransform>();
+        
+        rect.offsetMax += VectorOffset;
+        rect.offsetMin += VectorOffset;
+
+        if (anticipation)
+        {
+            offsetX *= 0.98f;
+            if (offsetX <= 0.01) anticipation = false;
+        }
+        else
+        {
+  
+            offsetX = Mathf.Abs(offsetX);
+            offsetX *= 1.5f;
+            
+
+        }
+   
+        
+
+        if (offsetX > 1000) Destroy(gameObject);
+        
+    }
+}
