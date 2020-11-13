a8279359e9b9ed53f76d1e13205f8e7b2a149706
Jimmie Lundin <jimmie.lundin@student.forsbergsskola.se>
2020-11-13 11:31:38 +0100

    climbing squirrel added and moves

---
+++ Assets/Scripts/AlternativePathMovement/FollowPath.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class FollowPath : MonoBehaviour
+{
+    public enum MovementType
+    {
+        MoveTowards,
+        LerpTowards
+    }
+
+    public MovementType Type = MovementType.MoveTowards; 
+    public MovementPath MyPath; 
+    public float Speed = 1; 
+    public float MaxDistanceToGoal = .1f;
+    
+    private IEnumerator<Transform> pointInPath;
+
+    public void Start()
+    {
+        if (MyPath == null)
+        {
+            return;
+        }
+        
+        pointInPath = MyPath.GetNextPathPoint();
+        pointInPath.MoveNext();
+
+        if (pointInPath.Current == null)
+        {
+            return; 
+        }
+        transform.position = pointInPath.Current.position;
+    }
+    
+    public void Update()
+    {
+        if (pointInPath == null || pointInPath.Current == null)
+        {
+            return; 
+        }
+
+        if (Type == MovementType.MoveTowards) 
+        {
+            transform.position = Vector3.MoveTowards(transform.position, pointInPath.Current.position, Time.deltaTime * Speed);
+        }
+        else if (Type == MovementType.LerpTowards) 
+        {
+            transform.position = Vector3.Lerp(transform.position, pointInPath.Current.position, Time.deltaTime * Speed);
+        }
+
+        var distanceSquared = (transform.position - pointInPath.Current.position).sqrMagnitude;
+        if (distanceSquared < MaxDistanceToGoal * MaxDistanceToGoal) 
+        {
+            pointInPath.MoveNext(); 
+        }
+    }
+}
---
+++ Assets/Scripts/ClimbingSquirrel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClimbingSquirrel : MonoBehaviour
+{
+    public float speed;
+    private bool movingUp = true;
+    private bool collided;
+
+    public void Movement()
+    {
+        if (collided)
+        {
+            transform.Translate(Vector2.down * speed * Time.deltaTime, Space.World);
+        }
+        else if (!collided) transform.Translate(Vector2.up * speed * Time.deltaTime, Space.World);
+    }
+
+    void Update()
+    {
+        Movement();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (movingUp == true)
+        {
+            transform.eulerAngles = new Vector3(180, 0, 0);
+            movingUp = false;
+            collided = true;
+        }
+        else
+        {
+            transform.eulerAngles = new Vector3(0, 0, 0);
+            movingUp = true;
+            collided = false;
+        }
+    }
+}
