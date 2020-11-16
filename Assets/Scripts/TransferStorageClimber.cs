f603305254670f48280bd24485c159208c90a433
jimmiels <jimmie.lundin@student.forsbergsskola.se>
2020-11-16 12:23:02 +0100

    Refactor turning squirrel

--- Assets/Scripts/TransferStorageClimber.cs
+++ Assets/Scripts/TransferStorageClimber.cs
@@ -23,6 +23,7 @@ public class TransferStorageClimber : MonoBehaviour {
         if (squirrel.isWaiting) {
             ElevatorStorageTransfer();
         }
+        TurnAroundIfFull();
     }
 
 
@@ -78,6 +79,15 @@ public class TransferStorageClimber : MonoBehaviour {
             StartCoroutine(startTransfer());
             var possibleTakeAmaount = myStorage.capacity - myStorage.currentAmount;
             myStorage.currentAmount += possibleTakeAmaount;
+        }
+    }
+    private void TurnAroundIfFull()
+    {
+        if (myStorage.currentAmount >= myStorage.capacity)
+        {
+            transform.eulerAngles = new Vector3(180, 0, 0);
+            squirrel.movingUp = false;
+            squirrel.collided = true;
         }
     }
 }
