72b09421a2adffd41e36a213884b485af5390ae3
jimmiels <jimmie.lundin@student.forsbergsskola.se>
2020-11-16 16:46:31 +0100

    Revert "Merge branch 'main' of https://github.com/TreeSloths/IdleClickerGroup into main"
    This reverts commit 0920775e57c9f417466b0bc2275effff95823b6a, reversing
    changes made to da16dd23d020c217045f5d920828e720255c5848.

--- Assets/Scripts/UpgradeSquirrel.cs
+++ Assets/Scripts/UpgradeSquirrel.cs
@@ -2,78 +2,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class UpgradeSquirrel : MonoBehaviour {
     private SquirrelMovement squirrel;
     public Resource nuts;
-
-
-    public float priceMultiplier = 5f;
-    public float speedModifier = 1.1f;
-    private Storage storage;
-    private TransferStorage transfer;
-    public Text labelText;
-
-    public float SquirrelSpeed {
-        get => PlayerPrefs.GetFloat(squirrel.name + "Speed", 1.5f);
-        set {
-            PlayerPrefs.SetFloat(squirrel.name + "Speed", value);
-            squirrel.speed = value;
-        }
-    }
-
-    public int Price {
-        get => PlayerPrefs.GetInt(squirrel.name + "UpgradePrice", 400);
-        set => PlayerPrefs.SetInt(squirrel.name + "UpgradePrice", value);
-    }
-
-    public int Level {
-        get => PlayerPrefs.GetInt(squirrel.name + "Level", 0);
-        set => PlayerPrefs.SetInt(squirrel.name + "Level", value);
-    }
-
-    public int StorageCapacity {
-        get => PlayerPrefs.GetInt(squirrel.name + "Capacity", 20);
-        set {
-            PlayerPrefs.SetInt(squirrel.name + "Capacity", value);
-            storage.capacity = value;
-        }
-    }
-
-    public float WaitTimer {
-        get => PlayerPrefs.GetFloat(squirrel.name + "waitTime", 2f);
-        set {
-            PlayerPrefs.SetFloat(squirrel.name + "waitTime", value);
-            transfer.waitTimer = value;
-        }
-    }
+    public int price;
+    public float priceMultiplier = 1.1f;
+    public float speedModifier;
+    
 
     private void Start() {
         squirrel = GetComponent<SquirrelMovement>();
-        storage = squirrel.GetComponent<Storage>();
-        transfer = squirrel.GetComponent<TransferStorage>();
-        labelText.text = $"Upgrade for {Price}\n lvl {Level}";
-        storage.capacity = StorageCapacity;
-        transfer.waitTimer = WaitTimer;
-        squirrel.speed = SquirrelSpeed;
-    }
-
-    private void Update() {
-    }
-
-    public void PurchaseUpgrade() {
-        if (nuts.ResourceAmount < Price) {
-            return;
-        } else {
-            nuts.ReduceResource(Price);
-            SquirrelSpeed *= speedModifier;
-            squirrel.speed = SquirrelSpeed;
-            StorageCapacity += 50;
-            WaitTimer -= 0.2f;
-            Price *= Mathf.RoundToInt(priceMultiplier);
-            Level++;
-            labelText.text = $"Upgrade for {Price}\n lvl {Level}";
-        }
+        
+        
     }
 }
