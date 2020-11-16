da16dd23d020c217045f5d920828e720255c5848
jimmiels <jimmie.lundin@student.forsbergsskola.se>
2020-11-16 16:39:30 +0100

    Adds and removes NutPiles when full/empty

---
+++ Assets/Scripts/EnableNutPile.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnableNutPile : MonoBehaviour
+{
+    private Storage storageContainer;
+
+    void Start()
+    {
+        storageContainer = GetComponent<Storage>();
+        GetComponent<SpriteRenderer>().enabled = false;
+    }
+
+
+    void Update()
+    {
+        if (storageContainer.currentAmount > 0)
+        {
+            GetComponent<SpriteRenderer>().enabled = true;
+        }
+        else
+        {
+            GetComponent<SpriteRenderer>().enabled = false;
+        }
+    }
+}
