c37c7e34809b9d79060f428b6be76ce07ba7abfb
Kevin Lempa <kevin.lempa@student.forsbergsskola.se>
2020-11-18 10:41:35 +0100

    saves all the squirrels to playerprefs

--- Assets/Scripts/Boosters.cs
+++ Assets/Scripts/Boosters.cs
@@ -14,32 +14,27 @@ public class Boosters : MonoBehaviour {
     public List<TransferStorage> transfer = new List<TransferStorage>();
     public ClimbingSquirrel climbingSquirrel;
     public TransferStorageClimber transferClimber;
+    public float timeModifier = .2f;
     public Storage climberStorage;
     public int price = 1000;
-    private List<float> originalSpeed = new List<float>(new[] {0f, 0f, 0f, 0f});
-    private List<int> origianlStorageValue = new List<int>(new[] {0, 0, 0, 0 ,0});
-    private List<float> originalWaitTimer = new List<float>(new[] {0f, 0f, 0f, 0f});
-    private List<int> origianlElevatorStorageValue = new List<int>(new[] {0, 0, 0, 0, 0});
 
-
-    private float originalClimbSpeed;
-    private float originClimbWaitTime;
+    
 
     
     IEnumerator BoostFor1MIN() {
         yield return new WaitForSeconds(60f);
-        for (var i = 0; i < 4; i++) {
-            squirrels[i].speed = originalSpeed[i];
-            transfer[i].waitTimer = originalWaitTimer[i];
-            storages[i].capacity = origianlStorageValue[i];
+        for (var i = 0; i < squirrels.Count; i++) {
+            squirrels[i].speed -= speedModifier;
+            transfer[i].WaitTimer += timeModifier;
+            storages[i].Capacity -= capacitymodifier;
         }
 
-        for (var i = 0; i < 4; i++) {
-            elevatorStorages[i].capacity = origianlElevatorStorageValue[i];
+        for (var i = 0; i < elevatorStorages.Count; i++) {
+            elevatorStorages[i].Capacity = capacitymodifier;
         }
 
-        climbingSquirrel.speed = originalClimbSpeed;
-        transferClimber.waitTimer = originClimbWaitTime;
+        climbingSquirrel.speed -= speedModifier;
+        transferClimber.waitTimer += timeModifier;
     }
 
     public void PurchaseBooster() {
@@ -47,30 +42,25 @@ public class Boosters : MonoBehaviour {
             return;
         } else {
             nuts.ReduceResource(price);
-            for (var i = 0; i < 4; i++) {
-                origianlStorageValue[i] = storages[i].capacity;
-                storages[i].capacity += capacitymodifier;
+            for (var i = 0; i < storages.Count; i++) {
+                storages[i].Capacity += capacitymodifier;
             }
 
-            for (var i = 0; i < 4; i++) {
-                originalSpeed[i] = squirrels[i].speed;
-                squirrels[i].speed *= speedModifier;
+            for (var i = 0; i < squirrels.Count; i++) {
+                squirrels[i].speed += speedModifier;
             }
 
-            for (var i = 0; i < 4; i++) {
-                originalWaitTimer[i] = transfer[i].waitTimer;
-                transfer[i].waitTimer -= 0.2f;
+            for (var i = 0; i < transfer.Count; i++) {
+                transfer[i].WaitTimer -= timeModifier;
             }
 
-            for (var i = 0; i < 4; i++) {
-                origianlElevatorStorageValue[i] = elevatorStorages[i].capacity;
-                elevatorStorages[i].capacity += capacitymodifier;
+            for (var i = 0; i < elevatorStorages.Count; i++) {
+                elevatorStorages[i].Capacity += capacitymodifier;
             }
 
-            originalClimbSpeed = climbingSquirrel.speed;
-            climbingSquirrel.speed *= speedModifier;
-            originClimbWaitTime = transferClimber.waitTimer;
-            transferClimber.waitTimer -= 0.2f;
+           
+            climbingSquirrel.speed += speedModifier;
+            transferClimber.waitTimer -= timeModifier;
 
             StartCoroutine(BoostFor1MIN());
         }
