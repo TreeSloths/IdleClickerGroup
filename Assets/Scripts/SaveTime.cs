bbf45c1feaefe463b975b65278e2bdbb85adb1b8
Adam Alzuhairi <adam_alzu@hotmail.com>
2020-11-18 15:39:25 +0100

    Updated script, should be finished with everything soon

--- Assets/Scripts/SaveTime.cs
+++ Assets/Scripts/SaveTime.cs
@@ -9,7 +9,7 @@ public class SaveTime : MonoBehaviour
     public DateTime systemQuit;
     private readonly DateTime startOfTime = new DateTime(1970, 1, 1, 0, 0, 0);
 
-    public long NowMilliSeconds
+    public long Quitinseconds
     {
         get
         {
@@ -17,11 +17,17 @@ public class SaveTime : MonoBehaviour
             return Convert.ToInt64(converting.Subtract(this.startOfTime).TotalSeconds);
         }
     }
+    
+    public long NowinSeconds {
+        get {
+            return Convert.ToInt64(DateTime.UtcNow.Subtract(this.startOfTime).TotalSeconds);
+        }
+    }
 
     private void Start()
     {
         systemStart = DateTime.Now;
-        Debug.Log($"{NowMilliSeconds}");
+        Debug.Log($"{NowinSeconds}");
     }
 
     private void OnApplicationQuit()
--- Assets/UI/PopUpMenu/PopUpMenu.cs
+++ Assets/UI/PopUpMenu/PopUpMenu.cs
@@ -10,19 +10,21 @@ public class PopUpMenu : MonoBehaviour
     public Text popUpMenuText;
     public SaveTime SaveTime;
 
-    private double comparing1;
+    private long comparing1;
     private double comparing2;
     private double comparing3;
     
     
     void Start()
     {
+        // nu minus då
         
-       comparing1 = Convert.ToDouble(System.DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss"));
-       comparing2 = Convert.ToDouble(SaveTime.systemQuit);
-       comparing3 = comparing1 - comparing2;
+        
 
-        this.popUpMenuText.text = $"Welcome back, this is the current time of your system: {comparing3}";
+
+
+        this.popUpMenuText.text = $"Welcome back, This is the time you were last online: {SaveTime.timeSaveQuit}" +
+                                  $"Test";
     }
     
     void Update()
