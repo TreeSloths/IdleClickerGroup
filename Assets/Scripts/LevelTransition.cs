f436ebc99fcc7b53e57c5495c578a33e90d9e8a2
Kevin Lempa <kevin.lempa@student.forsbergsskola.se>
2020-11-19 10:59:03 +0100

    Revert "Revert "HomeTreeSceneAdd""
    This reverts commit 76783b2844c76a32f36cc6880250934a22c76beb.

---
+++ Assets/Scripts/LevelTransition.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class LevelTransition : MonoBehaviour
+{
+   
+public void LoadNextLevel()
+    {
+        SceneManager.LoadScene("HomeTree");
+    }
+
+    public void LoadPreviousLevel()
+    {
+        SceneManager.LoadScene("TestSceneJimmie");
+    }
+
+}
