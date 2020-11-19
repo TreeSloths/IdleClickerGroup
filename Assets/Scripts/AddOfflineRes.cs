95aab6d7ac9de0548620828ac0182b784d947c1f
Adam Alzuhairi <adam_alzu@hotmail.com>
2020-11-19 19:25:04 +0100

    Made the welcome screen better looking and fixed addofflineres plus savetime

--- Assets/Scripts/AddOfflineRes.cs
+++ Assets/Scripts/AddOfflineRes.cs
@@ -10,28 +10,49 @@ public class AddOfflineRes : MonoBehaviour
     public float offlineRes = 0.6f;
     public double production;
     public long nowMinusThen;
+    public int amountOfBoostFour;
 
     private void Start()
     {
         nowMinusThen = SaveTime.NowinSeconds - SaveTime.Quitinseconds;
-        Debug.Log(nowMinusThen);
+        //   Debug.Log(nowMinusThen);
         production = nowMinusThen * offlineRes;
         this.Resource.ResourceAmount += Mathf.RoundToInt((float) production);
-        nowMinusThenSave = nowMinusThen.ToString();
+        Debug.Log($"{nowMinusThenSave}");
+        nowMinusThenSave += Convert.ToInt32(nowMinusThen);
     }
-    
-    public string nowMinusThenSave
+
+    private void OnApplicationQuit()
     {
-        get => PlayerPrefs.GetString("MinusSaved", "");
-        set => PlayerPrefs.SetString("MinusSaved", value);
+        
     }
-    
-    private void boostFourHours()
+
+    public int nowMinusThenSave
     {
-        while (Convert.ToInt64(nowMinusThenSave) != 14400)
+        get => PlayerPrefs.GetInt("MinusSaved", 0);
+        set => PlayerPrefs.SetInt("MinusSaved", value);
+    }
+
+
+    public void OnBoosterClickFour()
+    {
+        amountOfBoostFour++;
+    }
+
+
+    public void boostFourHours()
+    {
+        if (Convert.ToInt64(nowMinusThenSave) <= 14400)
         {
             var boost = offlineRes * 4;
         }
     }
-    
+
+    public void boostTwoHours()
+    {
+        if (Convert.ToInt64(nowMinusThenSave) <= 7200)
+        {
+            var boost = offlineRes * 2;
+        }
+    }
 }
