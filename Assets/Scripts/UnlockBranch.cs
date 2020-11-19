742ec3850040306faae64bb9eed1d490c2008ca8
Kevin Lempa <kevin.lempa@student.forsbergsskola.se>
2020-11-19 11:24:48 +0100

    test

--- Assets/Scripts/UnlockBranch.cs
+++ Assets/Scripts/UnlockBranch.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,16 +7,43 @@ public class UnlockBranch : MonoBehaviour
 {
     public Resource nuts;
     public GameObject rollingSquirrel;
-    private bool isUnlocked;
+
+    public bool IsUnlocked {
+        get => intToBool(PlayerPrefs.GetInt(name + "bool", 0));
+        set => PlayerPrefs.SetInt(name + "bool", boolToInt(value));
+    }
+    
+
+    private void Update() {
+        if (IsUnlocked) {
+            rollingSquirrel.SetActive(true);
+        }
+    }
+
+    int boolToInt(bool val)
+    {
+        if (val)
+            return 1;
+        else
+            return 0;
+    }
+
+    bool intToBool(int val)
+    {
+        if (val != 0)
+            return true;
+        else
+            return false;
+    }
     public int price;
     
     public void BranchUnlocked()
     {
-        if (nuts.ResourceAmount >= price && !isUnlocked)
+        if (nuts.ResourceAmount >= price && !IsUnlocked)
         {
             nuts.ReduceResource(price);
             rollingSquirrel.SetActive(true);
-            isUnlocked = true;
+            IsUnlocked = true;
         }
     }
 }
---
+++ Assets/UI/UpgradeMenu/MenuItemOpenAnim_Pop.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuItemOpenAnim_Pop : MonoBehaviour
+{
+    public bool enabled;
+
+    private bool PopIn;
+
+    public float ScaleSpeed = 1.2f;
+    public float maxScale = 1.1f;
+    public float retractSpeed = 0.95f;
+    
+    
+    public int FramesDelay = 0;
+    private int framesDelay;
+    private float scale = 0.4f;
+    bool done = false;
+    bool retract = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.GetComponent<Image>().enabled = this.enabled;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+     
+    }
+
+    private void FixedUpdate()
+    {
+        framesDelay--;
+        if (PopIn && framesDelay <= 0) ScaleUp();
+    }
+
+    public void TabEnable()
+    { 
+        this.enabled = true;
+        
+        scale = 0.4f;
+        retract = false;
+        done = false;
+        PopIn = true;
+
+        framesDelay = FramesDelay;
+    }
+    public void TabDisable()
+    {
+        this.enabled = false;
+        this.GetComponent<Image>().enabled = this.enabled;
+    }
+    
+    
+    public void ScaleUp()
+    {
+        this.GetComponent<Image>().enabled = this.enabled;
+        transform.localScale = new Vector3(scale, scale, scale);
+        if (!done)
+        {
+            if (!retract)
+            {
+                scale = Mathf.Clamp(scale, 0, maxScale);
+                scale *= ScaleSpeed;
+                if (scale >= maxScale) retract = true;
+                
+            }
+            else
+            {
+                transform.localScale = new Vector3(scale, scale, scale);
+                scale *= retractSpeed;
+                if (scale <= 1)
+                {
+                    scale = 1;
+                    done = true;
+                    PopIn = false;
+                }
+            }
+        }
+    }
+    
+}
