39b764f14e132986502cd16e270088b90f200018
Oskar Jansson <oskar_slim89@hotmail.com>
2020-11-20 00:14:55 +0100

    Adds some VFX

---
+++ Assets/Models/VFX/scrollingClouds.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scrollingClouds : MonoBehaviour
+{
+    private float currentscroll;
+    private float speed = 0.03f;
+    void Start()
+    {
+    
+    }
+
+    void Update()
+    { 
+        var _material = GetComponent<SpriteRenderer>().material;
+        currentscroll += speed * Time.deltaTime;
+        _material.mainTextureOffset = new Vector2(currentscroll, 0);
+    }
+}
