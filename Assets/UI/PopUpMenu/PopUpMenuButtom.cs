444c7617c63ed047e0a14376f371aaa2df473635
Adam Alzuhairi <adam_alzu@hotmail.com>
2020-11-20 10:56:30 +0100

    script update

--- Assets/UI/PopUpMenu/PopUpMenuButtom.cs
+++ Assets/UI/PopUpMenu/PopUpMenuButtom.cs
@@ -7,11 +7,9 @@ using UnityEngine.UIElements;
 public class PopUpMenuButtom : MonoBehaviour
 {
     public GameObject popUpMenu;
-    public ShowWelcomeMenuOnceTracker ShowWelcomeMenuOnceTracker;
 
     public void ClosePopUp()
     {
         popUpMenu.SetActive(false);
-        ShowWelcomeMenuOnceTracker.ShowWelcomeOnce = 1;
     }
 }
