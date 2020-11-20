7914455df8b81c633c007e3a07d7789f2f673b1a
Adam Alzuhairi <adam_alzu@hotmail.com>
2020-11-20 09:00:13 +0100

    additional fixes to offlinerse script and panel made to prefab

--- Assets/Scripts/AddOfflineRes.cs
+++ Assets/Scripts/AddOfflineRes.cs
@@ -5,33 +5,98 @@ using UnityEngine;
 
 public class AddOfflineRes : MonoBehaviour
 {
-    public SaveTime SaveTime;
-    public Resource Resource;
+    public SaveTime saveTime;
+    public Resource resource;
     public float offlineRes = 0.6f;
     public double production;
     public long nowMinusThen;
 
     private void Start()
     {
-        nowMinusThen = SaveTime.NowinSeconds - SaveTime.Quitinseconds;
-        Debug.Log(nowMinusThen);
-        production = nowMinusThen * offlineRes;
-        this.Resource.ResourceAmount += Mathf.RoundToInt((float) production);
-        nowMinusThenSave = nowMinusThen.ToString();
-    }
-    
-    public string nowMinusThenSave
-    {
-        get => PlayerPrefs.GetString("MinusSaved", "");
-        set => PlayerPrefs.SetString("MinusSaved", value);
-    }
-    
-    private void boostFourHours()
-    {
-        while (Convert.ToInt64(nowMinusThenSave) != 14400)
+        nowMinusThen = saveTime.NowinSeconds - saveTime.Quitinseconds;
+        if (AmountofBoostsFour == 0 && AmountofBoostsTwo == 0)
         {
-            var boost = offlineRes * 4;
+            production = nowMinusThen * offlineRes;
+            this.resource.ResourceAmount += Mathf.RoundToInt((float) production);
+        }
+        else
+        {
+            return;
+        }
+
+        Debug.Log($"{nowMinusThenSaveFour}");
+        Debug.Log($"{nowMinusThenSaveTwo}");
+
+        if (AmountofBoostsFour >= 1)
+        {
+            nowMinusThenSaveFour += Convert.ToInt32(nowMinusThen);
+            if (Convert.ToInt64(nowMinusThenSaveFour) <= 14400)
+            {
+                production = nowMinusThen * offlineRes * 4;
+                this.resource.ResourceAmount += Mathf.RoundToInt((float) production);
+            }
+            else if (Convert.ToInt64(nowMinusThenSaveFour) >= 14400)
+            {
+                AmountofBoostsFour--;
+            }
+        }
+
+        if (AmountofBoostsTwo >= 1)
+        {
+            nowMinusThenSaveTwo += Convert.ToInt32(nowMinusThen);
+            if (Convert.ToInt64(nowMinusThenSaveTwo) <= 7200)
+            {
+                production = nowMinusThen * offlineRes * 2;
+                this.resource.ResourceAmount += Mathf.RoundToInt((float) production);
+            }
+        }
+        else if (Convert.ToInt64(nowMinusThenSaveTwo) >= 7200)
+        {
+            AmountofBoostsTwo--;
         }
     }
-    
+
+    public int AmountofBoostsFour
+    {
+        get => PlayerPrefs.GetInt("FoorHourBoostsSaved", 0);
+        set => PlayerPrefs.SetInt("FoorHourBoostsSaved", value);
+    }
+
+    public int AmountofBoostsTwo
+    {
+        get => PlayerPrefs.GetInt("TwoHoursBoostsSaved", 0);
+        set => PlayerPrefs.SetInt("TwoHoursBoostsSaved", value);
+    }
+
+
+    public int nowMinusThenSaveFour
+    {
+        get => PlayerPrefs.GetInt("MinusSavedForFourHoursBoosts", 0);
+        set => PlayerPrefs.SetInt("MinusSavedForFourHoursBoosts", value);
+    }
+
+    public int nowMinusThenSaveTwo
+    {
+        get => PlayerPrefs.GetInt("MinusSavedForTwoHoursBoosts", 0);
+        set => PlayerPrefs.SetInt("MinusSavedForTwoHoursBoosts", value);
+    }
+
+
+    public void OnBoosterClickFour()
+    {
+        if (resource.ResourceAmount >= 1000)
+        {
+            this.resource.ResourceAmount -= 1000;
+            AmountofBoostsFour++;
+        }
+    }
+
+    public void OnBoosterClickTwo()
+    {
+        if (resource.ResourceAmount >= 1000)
+        {
+            this.resource.ResourceAmount -= 1000;
+            AmountofBoostsTwo++;
+        }
+    }
 }
